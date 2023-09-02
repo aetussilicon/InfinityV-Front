b18f039a49ec513ffc7aa3254a59a60ec40cfc63
Artur <aetussilicon.proton.me>
2023-09-02 20:08:00 -0300

    Criação parcial da landing page

---
+++ src/container/Register/Register.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Helmet from 'react-helmet';
+import api from "../services/api";
+
+
+const Register = () => {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+
+        try {
+            const response = await api.post("/auth/register", {
+                email: email,
+                username: username,
+                password: password
+            });
+
+            console.log("Usuário cadastrado :)", response.data);
+        } catch (error) {
+            console.log("Erro de registro: ", error);
+        }
+    }
+
+    return (
+        <>
+            <Helmet>
+                <title>Criar Conta</title>
+            </Helmet>
+            <link rel="stylesheet" href="src\Register\Register.css" />
+            <body>
+                <section className="register-area">
+
+                    <div className="register">
+                        <div>
+                            <img src="/src/assets/logo.svg" alt="InfinityV logo" />
+                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="username">Nome de Usuário</label>
+                            <input
+                                type="text"
+                                name="username"
+                                placeholder="Josh" autoFocus
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)} />
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder="josh@gmail.com" autoFocus
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}/>
+
+                            <label htmlFor="password">Senha</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder=""
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}/>
+                            <label htmlFor="cfrnPassword">Confirmar senha</label>
+                            <input
+                                type="password"
+                                name="cfrnPassword"
+                                placeholder=""/>
+
+                            <input type="submit" value="criar conta" />
+                        </form>
+                        <p>Já possui conta? <Link to="/login">Logar</Link></p>
+                    </div>
+                </section>
+            </body>
+        </>
+    );
+
+}
+
+export default Register;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,27 @@
 import { Routes, BrowserRouter, Route } from "react-router-dom";
-import Login from "./Login/Login";
-import Vault from "./Vault/Vault";
-import Register from "./Register/Register"
+import { Fragment } from "react";
+import Home from "./container/Home/Home";
+import Login from "./container/Login/Login";
+import Register from "./container/Register/Register"
+import Vault from "./container/Vault/Vault"
+
+const Private =  ({Item}) => {
+    const signed  = false;
+
+    return signed > 0 ? Item : Register;
+};
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route Component={Vault} path="/"/>
-                <Route Component={Login} path="/login"/>
-                <Route Component={Register} path="/register" />
-            </Routes>
+            <Fragment>
+                <Routes>
+                    <Route Component={Home} path="/"/>
+                    <Route Component={Login} path="/login"/>
+                    <Route Component={Register} path="/register" />
+                    <Route Component={<Private Item={Vault}/>} path="/vault"/>
+                </Routes>
+            </Fragment>
         </BrowserRouter>
     );
 }
